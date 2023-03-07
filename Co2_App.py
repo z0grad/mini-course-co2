2d714997898299dcdcb8f3e3f017c55f28c56367
Zero Grad <112247214+z0grad@users.noreply.github.com>
2023-03-07 09:05:56 +0200

    Update Co2_App.py

--- Co2_App.py
+++ Co2_App.py
@@ -13,10 +13,10 @@ st.image('./co2.jpg')
 # Inputs
 engine_size = st.number_input('Engine Size', min_value=0.0, max_value=10.0, value=1.0)
 cylinders = st.number_input('Cylinders', min_value=0, max_value=10, value=1)
-Fuel_Consumption_Comb = st.number_input('Fuel Consumption Combined', min_value=0.0, max_value=100.0, value=1.0)
+Fuel_Consumption_City = st.number_input('Fuel Consumption City', min_value=0.0, max_value=100.0, value=1.0)
 
 # Output
-output = model.predict([[engine_size, cylinders, Fuel_Consumption_Comb]])
+output = model.predict([[engine_size, cylinders, Fuel_Consumption_City]])
 
 # Display the output
 st.write(' ## The predicted Co2 Emission is: ', round(output[0][0],2))
